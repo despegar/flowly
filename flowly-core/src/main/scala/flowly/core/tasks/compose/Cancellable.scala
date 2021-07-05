10276d4c956b7adeee8bf4d815c7497f5728e2f5
Nicolás Goreico <ngoreico@despegar.com>
2021-07-05 13:22:49 -0300

    Cancellable task

---
+++ flowly-core/src/main/scala/flowly/core/tasks/compose/Cancellable.scala
@@ -0,0 +1,19 @@
+package flowly.core.tasks.compose
+
+import flowly.core.context.{ExecutionContext, Key}
+import flowly.core.tasks.basic.Task
+import flowly.core.tasks.model.{Finish, TaskResult}
+
+trait Cancellable extends Task {
+
+  abstract override private[flowly] def execute(sessionId: String, executionContext: ExecutionContext): TaskResult = {
+    if(executionContext.get(CancelFlow).exists(identity)) Finish
+    else super.execute(sessionId, executionContext)
+  }
+
+  override def internalAllowedKeys: List[Key[_]] = List(CancelFlow) ++ super.internalAllowedKeys
+
+}
+
+case object CancelFlow extends Key[Boolean]
+
