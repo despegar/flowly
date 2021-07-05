168951201798f48267c7fe9ba34d5c9f8418ebba
Nicolás Goreico <ngoreico@despegar.com>
2021-07-05 16:56:44 -0300

    Workflow test with cancellable task

--- flowly-core/src/test/scala/flowly/core/workflow/WorkflowSpec.scala
+++ flowly-core/src/test/scala/flowly/core/workflow/WorkflowSpec.scala
@@ -1,16 +1,33 @@
 package flowly.core.workflow
 
 import flowly.core.context.ExecutionContextFactory
-import flowly.core.repository.InMemoryRepository
+import flowly.core.repository.model.Session
+import flowly.core.repository.model.Status.{CANCELLED, FINISHED}
+import flowly.core.repository.{InMemoryRepository, Repository}
+import flowly.core.serialization.Serializer
 import flowly.core.tasks.ExecutionTask
 import flowly.core.tasks.basic.{FinishTask, Task}
-import flowly.core.{Context, Workflow, tasks}
+import flowly.core.tasks.compose.CancelFlow
+import flowly.core._
+import org.specs2.matcher.{EitherMatchers, ResultMatchers}
 import org.specs2.mutable.Specification
 
-class WorkflowSpec extends Specification {
+
+class WorkflowSpec extends Specification with EitherMatchers with ResultMatchers {
+
+  private def buildWorkflow(serializer: Serializer): Workflow = {
+    val thirdTask = FinishTask("OK")
+    val secondTask = tasks.BlockingCancellableTask("Task2", thirdTask, ec => Right(ec.contains(BooleanKey)), List(BooleanKey))
+    val firstTask = tasks.ExecutionTask("Task1", secondTask){ case(_, ec) => Right(ec) }
+
+    new Workflow {
+      override def initialTask: Task = firstTask
+      override val executionContextFactory: ExecutionContextFactory = new ExecutionContextFactory(serializer)
+      override val repository: Repository = new InMemoryRepository
+    }
+  }
 
   "Workflow" should {
-
     "throw runtime exception" in new Context {
       val task2 = ExecutionTask("Task2", FinishTask("Task2")) { case (_, ec) => Right(ec) }
       val task1 = tasks.ExecutionTask("Task1", task2) { case (_, ec) => Right(ec) }
@@ -20,6 +37,51 @@ class WorkflowSpec extends Specification {
           override val executionContextFactory = new ExecutionContextFactory(serializer)
           override val repository = new InMemoryRepository
         } must throwAn[IllegalStateException]
+    }
+
+    "Finish OK" in new Context {
+      val workflow: Workflow = buildWorkflow(serializer)
+
+      val sessionOrError = for {
+        sessionId     <- workflow.init(Param(StringKey, "test"))
+        _             <- workflow.execute(sessionId, List.empty)
+        _             <- workflow.execute(sessionId, Param(BooleanKey, true))
+        session       <- workflow.repository.getById(sessionId)
+      } yield session
+
+      private val session: Session = sessionOrError.getOrElse(throw new RuntimeException("Session not found."))
+
+      session.status must_== FINISHED
+    }
+
+    "End as CANCELLED" in new Context {
+      val workflow: Workflow = buildWorkflow(serializer)
+
+      val sessionOrError = for {
+        sessionId     <- workflow.init(Param(StringKey, "test"))
+        _             <- workflow.execute(sessionId, List.empty)
+        _             <- workflow.execute(sessionId, Param(CancelFlow, true))
+        session       <- workflow.repository.getById(sessionId)
+      } yield session
+
+      private val session: Session = sessionOrError.getOrElse(throw new RuntimeException("Session not found."))
+
+      session.status must_== CANCELLED
+    }
+
+    "Fail trying to resume a cancelled workflow" in new Context {
+      val workflow: Workflow = buildWorkflow(serializer)
+
+      val sessionOrError = for {
+        sessionId     <- workflow.init(Param(StringKey, "test"))
+        _             <- workflow.execute(sessionId, List.empty)
+        _             <- workflow.execute(sessionId, Param(CancelFlow, true))
+        _             <- workflow.execute(sessionId, Param(BooleanKey, true))
+        session       <- workflow.repository.getById(sessionId)
+      } yield session
+
+      sessionOrError must beAnInstanceOf[Left[SessionCantBeExecuted, Session]]
+
     }
 
   }
