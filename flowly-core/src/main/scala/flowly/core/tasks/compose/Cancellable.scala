3d45801e3ec014047e1e4b35a786b2288eadc5f5
Nicolás Goreico <ngoreico@despegar.com>
2021-07-12 14:33:19 -0300

    You can now define tasks to execute after a cancellation order.

--- flowly-core/src/main/scala/flowly/core/tasks/compose/Cancellable.scala
+++ flowly-core/src/main/scala/flowly/core/tasks/compose/Cancellable.scala
@@ -1,18 +1,20 @@
 package flowly.core.tasks.compose
 
 import flowly.core.context.{ExecutionContext, Key}
-import flowly.core.tasks.basic.Task
-import flowly.core.tasks.model.{Cancel, Finish, TaskResult}
+import flowly.core.tasks.basic.{CancelTask, Task}
+import flowly.core.tasks.model.TaskResult
 
 trait Cancellable extends Task {
 
   abstract override private[flowly] def execute(sessionId: String, executionContext: ExecutionContext): TaskResult = {
-    if(executionContext.get(CancelFlow).exists(identity)) Cancel
+    if(executionContext.get(CancelFlow).exists(identity)) followedByAfterCancel.execute(sessionId, executionContext)
     else super.execute(sessionId, executionContext)
   }
 
   override def internalAllowedKeys: List[Key[_]] = List(CancelFlow) ++ super.internalAllowedKeys
 
+  //Override to add chained tasks to do stuff before cancelling. Then define CancelTask as next task there.
+  protected def followedByAfterCancel: Task = CancelTask
 }
 
 case object CancelFlow extends Key[Boolean]
