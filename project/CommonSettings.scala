d02efa2f9ff5031afbc74af5a6572849718e26cc
Nicolás Goreico <ngoreico@despegar.com>
2021-08-24 18:03:01 -0300

    new nexus

--- project/CommonSettings.scala
+++ project/CommonSettings.scala
@@ -5,13 +5,11 @@ object CommonSettings {
 
   val settings: Seq[Def.Setting[_]] =
       Seq(organization := "com.despegar.flowly",
-          publishTo := Some("Nexus Despegar" at s"http://nexus:8080/nexus/content/repositories/${if (isSnapshot.value) "snapshots" else "releases"}"),
+          publishTo := Some("Nexus Despegar" at s"https://cnd-backend/nexus/repository/${if (isSnapshot.value) "maven-snapshots" else "maven-releases"}/"),
           resolvers += Opts.resolver.mavenLocalFile,
           resolvers += Resolver.mavenLocal,
           resolvers += "Typesafe repository" at "https://repo.typesafe.com/typesafe/releases/",
-          resolvers += "Nexus Public Repository" at "http://nexus:8080/nexus/content/groups/public",
-          resolvers += "Nexus Snapshots Repository" at "http://nexus:8080/nexus/content/repositories/snapshots",
-          resolvers += "Nexus Proxies Repository" at "http://nexus:8080/nexus/content/groups/proxies",
+          resolvers += "Nexus Public Repository" at "https://cnd-backend/nexus/repository/maven-all/",
           scalaVersion := "2.13.0",
           crossScalaVersions := Seq("2.12.8", "2.13.0"))
 }
